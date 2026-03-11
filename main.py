c6f9f22525455e6d30064c80fc0aef705b220b9f
sanay-d-nyu <sd5113@nyu.edu>
2026-03-11 02:32:32 -0400

    Parse pegs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import dpll
+import numpy as np
 
 """
 NOTES:
@@ -12,7 +13,7 @@ def Puzzle2SAT(
     board: list[list[int]], start: list[int], numberOfJumps: int
 ) -> tuple[list[list[int]], list[list[int]]]:
 
-    clauses: list[list[int]] = []
+    # Get all possible jumps, plus Jump(0, 0, 0, 0)
     jump_atoms: list[list[int]] = [[0, 0, 0, 0]]
     possible_jumps: list[list[int]] = []
     for i in range(numberOfJumps):
@@ -20,6 +21,18 @@ def Puzzle2SAT(
         possible_jumps.extend(map(lambda triple: [*triple[::-1], i], board))
     jump_atoms.extend(possible_jumps)
 
+    # Generate clauses
+    clauses: list[list[int]] = []
+    num_jumps_arr = np.arange(numberOfJumps + 1)
+    vertices = np.unique(np.array(board))
+    pegs: list[list[int]] = np.transpose(
+        [np.tile(vertices, numberOfJumps + 1), np.repeat(num_jumps_arr, len(vertices))]
+    ).tolist()
+    # Cartesian product of [0..=numberOfJumps] x [0..=number of unique vertices (holes)]
+    # i.e, each possible Peg() token, Peg(0, 0)..Peg(num_holes, numberOfJumps)
+
+    print("pegs:")
+    print_helper(pegs)
     return ([[0]], jump_atoms)
 
 
@@ -43,6 +56,25 @@ def PegPuzzle(board,start,numberOfJumps):
 """
 
 
+def arr_to_string(arr1d):
+    n = len(arr1d) - 1
+    ret_str = "["
+    for i, v in enumerate(arr1d):
+        ret_str += str(v)
+        if i != n:
+            ret_str += ", "
+    return ret_str + "]"
+
+
+def print_helper(arr2d):
+    print("[")
+    for arr in arr2d:
+        print(
+            f"\t{arr_to_string(arr)},",
+        )
+    print("]")
+
+
 def main():
     with open("input.json", "r") as file:
         input_data = json.load(file)
@@ -61,13 +93,17 @@ def main():
     # found, bindings = dpll.DPLLTop(clauses)
 
     # print(f"board: {board}, start: {start}, num_jumps: {num_jumps}")
-    print(
-        f"clauses_from_input: {clauses_from_input}, jump_atoms_from_input: {jump_atoms_from_input}\n"
-    )
+    print("Clauses from input:")
+    print_helper(clauses_from_input)
+    print("Jump atoms from input:")
+    print_helper(jump_atoms_from_input)
     # print()
     print()
     clauses, jump_atoms = Puzzle2SAT(board, start, 2)
-    print(f"clauses: {clauses}, jump_atoms: {jump_atoms}\n")
+    print("Clauses:")
+    print_helper(clauses)
+    print("Jump atoms:")
+    print_helper(jump_atoms)
     # print(f"found: {found}, bindings {bindings}, len(bindings): {len(bindings)}\n")
 
 
